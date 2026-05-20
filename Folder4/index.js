0c6c89886464fac03390d1f3a7762a977bcbb55f
dovudbek7 <dovudbekm99@gmail.com>
2026-05-20 15:22:43 +0500

    add

---
+++ Folder4/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hellow world");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send("Unavailable course");
+  res.send(course);
+});
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.query);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().required().min(3),
+  });
+  const result = schema.validate(req.body);
+  console.log(result);
+
+  if (result.error) {
+    res.status(400).send(result.error);
+    return;
+  }
+
+  //   if (!req.body.name || req.body.name.length < 3) {
+  //     res.status(400).send("Name is required");
+  //     return;
+  //   }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+const port = 3000;
+// const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
