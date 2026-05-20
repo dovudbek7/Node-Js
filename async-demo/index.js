318461e703e99a32d32045c8e01b61663ac70209
dovudbek7 <dovudbekm99@gmail.com>
2026-05-20 15:22:43 +0500

    Falback

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,10 @@
 console.log("Before");
 getUser(1, function (user) {
   console.log("User", user);
+
+  getRepo("Username", (repo) => {
+    console.log("Repo: " + repo);
+  });
 });
 // console.log(user);
 console.log("After");
@@ -9,5 +13,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database");
     callback({ id: id, gitHubUsername: "mosh" });
+  }, 2000);
+}
+
+function getRepo(username, callback) {
+  setTimeout(() => {
+    console.log("Get repo");
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
