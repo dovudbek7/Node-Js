1d8acbf0817a489eadf30a07e73dbab6ba5bb58f
dovudbek7 <dovudbekm99@gmail.com>
2026-05-21 12:07:13 +0500

    13- Pagination

--- mongodb/index2.js
+++ mongodb/index2.js
@@ -14,25 +14,30 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model("Course", courseSchema);
 
 async function getCourses() {
+  const pageNumber = 2;
+  const pageSize = 10;
   return await Course
     // .find({ isPublished: true, tags: "backend" })
     // .find({ price: { $gte: 10, $lte: 15 } })
-    // .find()
+    .find()
     // .or([{ author: "Mosh" }, { isPublished: true }])
     // .and([{ author: "Mosh" }, { isPublished: true }])
     // .find({ price: { $in: [10, 20, 30] } })
 
     // Starts with Mosh
-    .find({ author: /^Mosh/ })
+    // .find({ author: /^Mosh/ })
 
     // Ends with Hamedani
-    .find({ author: /Hamedani$/i })
+    // .find({ author: /Hamedani$/i })
 
     // Contains Mosh
-    .find({ author: /.*Mosh.*/i })
+    // .find({ author: /.*Mosh.*/i })
 
-    .sort({ name: 1 })
-    .select({ name: 1, author: 1, price: 1 });
+    // .sort({ name: 1 })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    // .select({ name: 1, author: 1, price: 1 });
+    .countDocuments();
 }
 
 async function run() {
