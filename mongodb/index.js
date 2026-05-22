a14e6ef1e088e451212382cb21b80c341dd9ac37
dovudbek7 <dovudbekm99@gmail.com>
2026-05-22 14:42:15 +0500

    3- Custom Validators

--- mongodb/index.js
+++ mongodb/index.js
@@ -16,7 +16,15 @@ const courseSchema = new mongoose.Schema({
     enum: ["web", "mobile", "network"],
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0
+      },
+      message: "A Course should have at least one tag"
+    }
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
