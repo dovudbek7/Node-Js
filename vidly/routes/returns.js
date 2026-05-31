dffd7c1be21e37b1a41c5f0544ca0c0c56c0b759
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 18:42:06 +0500

    8- Looking Up an Object

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const {Rental} = require('../models/rental');
 const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
@@ -6,6 +7,13 @@ const router = express.Router();
 router.post('/', auth, async (req, res) => {
   const { error } = validateReturn(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  const rental = await Rental.findOne({
+    'customer._id': req.body.customerId,
+    'movie._id': req.body.movieId
+  });
+
+  if (!rental) return res.status(404).send('Rental not found.');
 });
 
 function validateReturn(req) {
