e075e01479342c9256e16ff65f366c43db2d5415
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 14:28:07 +0500

    4- Removing Try Catch Blocks

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require("../middleware/async");
 const { Genre, validate } = require("../models/genre");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
@@ -5,16 +6,12 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const genres = await Genre.find().sort("name");
-    res.send(genres);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", asyncMiddleware(async (req, res) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
+}));
 
-router.post("/", auth, async (req, res) => {
+router.post("/", auth, asyncMiddleware(async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -22,9 +19,9 @@ router.post("/", auth, async (req, res) => {
   genre = await genre.save();
 
   res.send(genre);
-});
+}));
 
-router.put("/:id", auth, async (req, res) => {
+router.put("/:id", auth, asyncMiddleware(async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -40,24 +37,24 @@ router.put("/:id", auth, async (req, res) => {
     return res.status(404).send("The genre with the given ID was not found.");
 
   res.send(genre);
-});
+}));
 
-router.delete("/:id", [auth, admin], async (req, res) => {
+router.delete("/:id", [auth, admin], asyncMiddleware(async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found.");
 
   res.send(genre);
-});
+}));
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", asyncMiddleware(async (req, res) => {
   const genre = await Genre.findById(req.params.id);
 
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found.");
 
   res.send(genre);
-});
+}));
 
 module.exports = router;
