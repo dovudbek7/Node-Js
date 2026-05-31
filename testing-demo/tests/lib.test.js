b9c6f1b2528d2a86ac44f6e530e6b8080b2ad241
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 17:24:41 +0500

    18- Jest Mock Functions

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -93,19 +93,32 @@ describe('applyDiscount', () => {
   });
 });
 
-describe('notifyCustomer', () => {
-  it('should send an email to the customer', () => {
-    db.getCustomerSync = function(customerId) {
-      return { email: 'a' };
-    }
+// describe('notifyCustomer', () => {
+//   it('should send an email to the customer', () => {
+//     db.getCustomerSync = function(customerId) {
+//       return { email: 'a' };
+//     }
 
-    let mailSent = false;
-    mail.send = function(email, message) {
-      mailSent = true;
-    }
+//     let mailSent = false;
+//     mail.send = function(email, message) {
+//       mailSent = true;
+//     }
+
+//     lib.notifyCustomer({ customerId: 1 });
+
+//     expect(mailSent).toBe(true);
+//   });
+// });
+
+describe("notifyCustomer", () => {
+  it("should send an email to the customer", () => {
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: "a" });
+    mail.send = jest.fn();
 
     lib.notifyCustomer({ customerId: 1 });
 
-    expect(mailSent).toBe(true);
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][0]).toBe("a");
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
