b54d145bcf2f8dee8caa82e3d41d5f70e9846758
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 11:59:11 +0500

    9- Generating Authentication Tokens

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -15,7 +16,8 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+  res.send(token);
 });
 
 function validate(req) {
