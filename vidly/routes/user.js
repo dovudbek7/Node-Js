f036d582c7a22d82b3d87f63b3a1086e9e777abf
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 12:37:29 +0500

    15- Getting the Current User

--- vidly/routes/user.js
+++ vidly/routes/user.js
@@ -1,8 +1,14 @@
 const { User, validate } = require("../models/user");
+const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
+
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
