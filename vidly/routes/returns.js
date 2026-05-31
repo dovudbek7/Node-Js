0e75b8816402e5f5513883708ec31d9a45e1370c
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 18:42:26 +0500

    9- Testing if Rental Processed

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -14,6 +14,8 @@ router.post('/', auth, async (req, res) => {
   });
 
   if (!rental) return res.status(404).send('Rental not found.');
+
+  if (rental.dateReturned) return res.status(400).send('Return already processed.');
 });
 
 function validateReturn(req) {
