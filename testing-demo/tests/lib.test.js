79b6cd277c8640ee9af6f05b227479aa32f85e25
dovudbek7 <dovudbekm99@gmail.com>
2026-05-31 16:03:08 +0500

    7- Testing Numbers

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,3 +1,19 @@
-test('Our first test', () => {
-    
-})
+const lib = require("../lib");
+
+// 1-test: Arrow funksiya (() =>) qo'shildi
+test("absolute - should return a positive number if input is positive", () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
+});
+
+// 2-test: Arrow funksiya (() =>) qo'shildi
+test("absolute - should return a positive number if input is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+// 3-test: Kutilayotgan natija (toBe) 1 emas, 0 bo'lishi kerak
+test("absolute - should return 0 if input is 0", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0); // 0 kiritilganda 0 qaytishi kerak
+});
